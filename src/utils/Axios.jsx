b19ba29ad435f11f4210f1b4d95028277e963797
SingleThreaded400 <av0232016@gmail.com>
2025-01-23 10:57:42 +0530

    Add files via upload

--- src/utils/Axios.jsx
+++ src/utils/Axios.jsx
@@ -1,1 +1,11 @@
+import axios from 'axios';
 
+const instance = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNmNhNzJmMzM3Y2JmZWMwOTRkZWIzYWNiNGVmNWQzOCIsIm5iZiI6MTczNTcwNzI5Ni4zOTgwMDAyLCJzdWIiOiI2Nzc0Y2FhMDJlZmJmM2I0MWQ5Mjk0NDYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.QC_jMYUAfi9iHFqVWgRYbD5ajfWAsZOtTceSVjnw0XU'
+  }
+});
+
+export default instance;
