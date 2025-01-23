179c14f11aad34e782e4d75c37cd3901f1a8829d
SingleThreaded400 <av0232016@gmail.com>
2025-01-23 10:54:25 +0530

    Add files via upload

--- src/store/Store.jsx
+++ src/store/Store.jsx
@@ -1,1 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit'
+import movieReducer from './reducers/MovieSlice.jsx';
+import tvReducer from './reducers/TvSlice.jsx';
+import personReducer from './reducers/PersonSlice.jsx';
 
+export const store = configureStore({
+  reducer: {
+      movie:movieReducer,
+      tv:tvReducer,
+      person:personReducer
+  },
+})
